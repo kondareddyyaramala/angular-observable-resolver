b1825507877656e2ba9eed6ffc0e198813ffcb80
kondareddyyaramala <kondareddy.yaramala@gmail.com>
2019-10-28 21:00:02 -0500

    Updated

--- src/app/auth-resolver.ts
+++ src/app/auth-resolver.ts
@@ -8,11 +8,11 @@ export class AuthResolver implements Resolve<boolean> {
     resolve(route: ActivatedRouteSnapshot): Observable<boolean> {
 
         // emits an obs after 2 seconds
-        let obs = timer(2000);
+        let observable = timer(2000);
 
         // It waits till the Obs gets resolved and then routes the user to the new
         // route
         // In real time it would be an service call
-        return obs.pipe(take(1), map(v => true));
+        return observable.pipe(take(1), map(v => true));
     }
 }
